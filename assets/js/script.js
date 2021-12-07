947778860732b7563fa3a05e078cda93f6548539
Xueting Hu <huxu@uwplatt.edu>
2021-12-07 00:01:36 -0600

    UV badge color

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
-  var apiUrl =
-    "https://api.openweathermap.org/data/2.5/weather?q=chicago&appid=818154f9875973e95a27ec8b0fc7191b";
+  var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=chicago&appid=818154f9875973e95a27ec8b0fc7191b";
   defaultCity(apiUrl);
 });
 
@@ -69,7 +68,7 @@ var displayCurrentCity = function (data) {
       $(".card-img" ).attr("src", "./assets/background/bg_night_clear.png");
       $(".current-icon").attr("src","./assets/icons/icon_sunny.png");
     }else{
-      $("#bg" + i).attr("src", "./assets/background/bg_night_mist.png");
+      $(".card-img").attr("src", "./assets/background/bg_night_mist.png");
       $(".current-icon").attr("src","./assets/icons/icon_mist.png");
     }
   }else if(moment().format('LT')[6] === 'A'){
@@ -89,7 +88,7 @@ var displayCurrentCity = function (data) {
       $(".card-img" ).attr("src", "./assets/background/bg_day_clear.png");
       $(".current-icon").attr("src","./assets/icons/icon_sunny.png");
     }else{
-      $("#bg" + i).attr("src", "./assets/background/bg_day_mist.png");
+      $(".card-img").attr("src", "./assets/background/bg_day_mist.png");
       $(".current-icon").attr("src","./assets/icons/icon_mist.png");
     }
   }
@@ -110,24 +109,16 @@ var getUVIndex = function (lat, lon) {
       response.json().then(function (data) {
         $(".currentUV").text(data.value);
         console.log(data);
-        if (data.value < 2 || data.value === 2) {
+        if (data.value < 2.01) {
           console.log('in')
           $(".currentUV").addClass("badge-sucess");
-        } else if (data.value > 2 && data.value < 5) {
+        } else if (data.value > 2 && data.value < 5.01) {
           $(".currentUV").addClass("badge-warning");
-        } else if (data.value === 5) {
-          $(".currentUV").addClass("badge-warning");
-        } else if (data.value > 5 && data.value < 7) {
+        } else if (data.value > 5 && data.value < 7.01) {
           $(".currentUV").addClass("badge-high");
-        } else if (data.value === 7) {
-          $(".currentUV").addClass("badge-high");
-        } else if (data.value > 7 && data.value < 10) {
+        } else if (data.value > 7 && data.value < 10.01) {
           $(".currentUV").addClass("badge-danger");
-        } else if (data.value === 10) {
-          $(".currentUV").addClass("badge-danger");
-        } else if (data.value > 11) {
-          $(".currentUV").addClass("badge-extreme");
-        } else if (data.value === 11) {
+        } else if (data.value > 10) {
           $(".currentUV").addClass("badge-extreme");
         }
       });
@@ -140,7 +131,7 @@ var displayForecast = function (city) {
 
   fetch(apiUrl).then(function (response) {
     if (response.ok) {
-      storeLocal(city);
+      //storeLocal(city);
 
       response.json().then(function (data) {  
         for (let i = 1; i < 6; i++) {
@@ -198,7 +189,7 @@ var displayForecast = function (city) {
   });
 };
 
-var storeLocal = function (userInput) {
+/*var storeLocal = function (userInput) {
   localStorage.setItem("record", JSON.stringify(userInput));
   var liEl = document.createElement("li");
   liEl.classList.add("list-group-item", "list-group-item-action");
@@ -210,9 +201,17 @@ var storeLocal = function (userInput) {
     }
   });
   document.querySelector(".search-history").appendChild(liEl);
+};*/
+
+let storedCities = JSON.parse(localStorage.getItem('storedCities')) || [];
+
+checkLocalStorage = () => {
+   storedCities = JSON.parse(localStorage.getItem('storedCities')) || [];
 };
 
+
 var displaySearchHistory = function (cityName) {
+  checkLocalStorage();
   for (let i = 0; i < localStorage.length; i++) {
     var liEl = $(
       `<button type='button' class='list-group-item list-group-item-action' id='${cityName}'>${cityName}</li>`
@@ -220,3 +219,16 @@ var displaySearchHistory = function (cityName) {
     liEl.appendTo(".search-history");
   }
 };
+
+var checkIfCityButtonExists = function(userInput){
+  checkLocalStorage();
+  let cityExists = false;
+  for (i = 0; i < storedCities.length; i++) {
+     if (city === storedCities[i]) {
+        cityExists = true;
+        return cityExists;
+     }
+  };
+  return cityExists;
+}
+
